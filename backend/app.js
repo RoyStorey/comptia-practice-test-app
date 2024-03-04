77acb36c9d408294c0cb71c3ea4edec35f758aca
Roy Storey <yourbuddyroy@gmail.com>
2024-03-04 16:20:43 +0000

    api key env

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,20 @@
-import express from 'express';
-import OpenAI from 'openai';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express from "express";
+import OpenAI from "openai";
+import cors from "cors";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 app.use(cors());
 app.use(express.json());
 const port = 3000;
 
-const openai = new OpenAI({ apiKey: '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' });
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-app.get('/api/get-question/:test/:objective', async (req, res) => {
+app.get("/api/get-question/:test/:objective", async (req, res) => {
   const testSelected = req.params.test;
   const objectiveSelected = req.params.objective;
-  let query = '';
+  let query = "";
 
   if (objectiveSelected) {
     query = `Give me one question from the ${testSelected} exam that is related to ${objectiveSelected}, and do not give me the answer. Do not give me multiple choice questions. I want a question that requires a written response. Err lightly on the side of correct.`;
@@ -21,20 +22,20 @@ app.get('/api/get-question/:test/:objective', async (req, res) => {
     query = `Give me one question from the ${testSelected} exam, and do not give me the answer. Do not give me multiple choice questions. I want a question that requires a written response. Err lightly on the side of correct.`;
   }
 
-  console.log('Request received')
+  console.log("Request received");
   const stream = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
     messages: [{ role: "user", content: query }],
     stream: true,
   });
-  let response = '';
+  let response = "";
   for await (const chunk of stream) {
     response += chunk.choices[0]?.delta?.content || "";
   }
   res.json({ data: response });
 });
 
-app.post('/api/post-user-answer', async (req, res) => {
+app.post("/api/post-user-answer", async (req, res) => {
   const { question, answer } = req.body;
   const query = `The question is: ${question}. The user's answer is: ${answer}. is this Correct | Incorrect | Partially Correct? if incorrect or partially correct, what is the correct answer, and provide feedback?`;
   const stream = await openai.chat.completions.create({
@@ -42,12 +43,12 @@ app.post('/api/post-user-answer', async (req, res) => {
     messages: [{ role: "user", content: query }],
     stream: true,
   });
-  let response = '';
+  let response = "";
   for await (const chunk of stream) {
     response += chunk.choices[0]?.delta?.content || "";
   }
   res.json({ data: response });
-})
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
